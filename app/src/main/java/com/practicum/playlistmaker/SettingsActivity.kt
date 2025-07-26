328a9d1366b661cedc807d639db424f870897c54
Svetlana Lopareva <seetochka95@gmail.com>
2025-07-26 14:37:11 +0500

    Fix naming and return to main

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.practicum.playlistmaker
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,11 +9,10 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val mainButton = findViewById<ImageView>(R.id.main)
+        val backButton = findViewById<ImageView>(R.id.main)
 
-        mainButton.setOnClickListener {
-            val mainIntent = Intent(this, MainActivity::class.java)
-            startActivity(mainIntent)
+        backButton.setOnClickListener {
+            finish()
         }
     }
 }
